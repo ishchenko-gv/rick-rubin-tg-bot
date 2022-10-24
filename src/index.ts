e1ddfcfa8a2084077e3c52c236b93f066e83ae58
George Ishchenko <metalkint@gmail.com>
2022-10-24 23:50:58 +0300

    dev

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,46 @@
 import 'reflect-metadata';
 import { Container } from 'typedi';
 import * as dotenv from 'dotenv';
-import { Telegram } from './components/telegram/telegram';
-import { RickService } from './components/rick/rick.service';
+
+import { Telegraf } from 'telegraf';
+import { Message } from 'typegram';
+import { RickService, ChatMessage } from './components/rick/rick.service';
 
 dotenv.config();
 
-new Telegram(Container.get(RickService)).start();
+class App {
+  private bot;
+  private rickService;
+
+  constructor(bot: Telegraf, rickService: RickService) {
+    this.bot = bot;
+    this.rickService = rickService;
+  }
+
+  start() {
+    this.bot.on('text', async (ctx) => {
+      const { text, reply_to_message } = ctx.message;
+
+      const chatMessage: ChatMessage = {
+        text,
+        replyToMessage: {
+          text: ((reply_to_message || { text: '' }) as Message.TextMessage).text
+        }
+      };
+
+      this.rickService.readMessage(chatMessage, (message) =>
+        ctx.reply(message)
+      );
+    });
+
+    this.bot.launch();
+
+    process.once('SIGINT', () => this.bot.stop('SIGINT'));
+    process.once('SIGTERM', () => this.bot.stop('SIGTERM'));
+  }
+}
+
+new App(
+  new Telegraf(process.env.BOT_TOKEN),
+  Container.get(RickService)
+).start();
